a5ecc444fbf0eebeca3c6d8ec427d714ede43c8d
louis de Domingo <louisjamesdedo@gmail.com>
2021-12-20 18:06:54 +0100

    changer le trucx

--- src/journal_lycee/views.py
+++ src/journal_lycee/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>tamere<h1>")
+    return HttpResponse("lorem isszpfihdiuofvgqhgsrguijqdsrmiogk")
 
 def mainmenu(request):
     return HttpResponse("<h1>test<h1>")
